47385ca702936884fcf21fdec6e44151bf4c7aa9
이승혁 <devmelonlee@gmail.com>
2022-12-30 16:59:11 +0900

    [CHORE] 프로젝트 세팅

---
+++ Exchange-Rate/Exchange-Rate/Exchange_RateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exchange_RateApp.swift
+//  Exchange-Rate
+//
+//  Created by 이승혁 on 2022/12/30.
+//
+
+import SwiftUI
+
+@main
+struct Exchange_RateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
