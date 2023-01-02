9d3b0c89337dcb5c752820feb7a0ef3a453e1343
이승혁 <devmelonlee@gmail.com>
2023-01-02 23:43:41 +0900

    [UI] 런치스크린 뷰 추가

---
+++ Exchange-Rate/Exchange-Rate/Sources/Views/LaunchScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchScreenView.swift
+//  Exchange-Rate
+//
+//  Created by 이승혁 on 2023/01/02.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Text("런치스크린뷰입니다.")
+    }
+}
+
+struct LaunchScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreenView()
+    }
+}
